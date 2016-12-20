9602cd548e54c4c02e94dfb2a65f31a0ff4655ba
Matthew Pirocchi <matthew.pirocchi@gmail.com>
2016-12-20 13:38:00 -0500

    Improve webpack.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,19 +1,26 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+import webpack from 'webpack'
+import path from 'path'
+
+const npmCommand = process.env.BABEL_ENV = process.env.npm_lifecycle_event
+const isProduction = npmCommand === 'production'
 
 const paths = {
-  app: __dirname + '/app/',
-  dist: __dirname + '/dist/',
+  app: path.join(__dirname, 'app'),
+  dist: path.join(__dirname, 'dist'),
 }
 
 const htmlWebpackPluginConfig = {
   title: 'Cardinal',
   filename: 'index.html',
-  template: paths.app + 'index.html',
+  template: path.join(paths.app, 'index.html'),
   inject: 'body',
 }
 
-const webpackConfig = {
-  entry: paths.app + 'index.js',
+const baseConfig = {
+  entry: [
+    paths.app
+  ],
   output: {
     path: paths.dist,
     filename: 'index_bundle.js',
@@ -27,14 +34,44 @@ const webpackConfig = {
       },
       {
         test: /\.css$/,
-        include: [ paths.app ],
-        loader: 'style!css',
+        loader: 'style!css?sourceMap&modules&localIdentName=[name]__[local]___[hash:base64:5]',
       },
     ],
   },
+  resolve: {
+    root: path.resolve('./app'),
+  },
+}
+
+const productionConfig = {
+  devtool: 'cheap-module-source-map',
   plugins: [
     new HtmlWebpackPlugin(htmlWebpackPluginConfig),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
   ],
 }
 
-export default webpackConfig
+const developmentConfig = {
+  devtool: 'cheap-module-inline-source-map',
+  devSever: {
+    contentBase: paths.dist,
+    hot: true,
+    inline: true,
+    progress: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin(htmlWebpackPluginConfig),
+    new webpack.HotModuleReplacementPlugin(),
+  ]
+}
+
+const finalConfig = {
+  ...baseConfig,
+  ...(isProduction ? productionConfig : developmentConfig)
+}
+
+export default finalConfig
