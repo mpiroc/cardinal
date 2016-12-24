b448af560b8577e94e135022a63982a5e79203a9
Matthew Pirocchi <matthew.pirocchi@gmail.com>
2016-12-24 13:48:29 -0500

    Add value fetching actions to decks module.

--- app/redux/rewrite/decks.js
+++ app/redux/rewrite/decks.js
@@ -1,15 +1,10 @@
 import { Map } from 'immutable'
 
-const UPDATE_DECK_VALUE = 'UPDATE_DECK_VALUE'
 const ADD_DECK_CARD = 'ADD_DECK_CARD'
 const REMOVE_DECK_CARD = 'REMOVE_DECK_CARD'
-
-function updateDeckValue(deck) {
-  return {
-    type: UPDATE_DECK_VALUE,
-    deck,
-  }
-}
+const SETTING_DECK_VALUE_LISTENER = 'SETTING_DECK_VALUE_LISTENER'
+const SETTING_DECK_VALUE_LISTENER_SUCCESS = 'SETTING_DECK_VALUE_LISTENER_SUCCESS'
+const SETTING_DECK_VALUE_LISTENER_FAILURE = 'SETTING_DECK_VALUE_LISTENER_FAILURE'
 
 function addDeckCard(deckId, cardId) {
   return {
@@ -27,7 +22,33 @@ function removeDeckCard(deckId, cardId) {
   }
 }
 
+function settingDeckValueListener(deckId) {
+  return {
+    type: SETTING_DECK_VALUE_LISTENER,
+    deckId,
+  }
+}
+
+function settingDeckValueListenerSuccess(deckId, deck) {
+  return {
+    type: SETTING_DECK_VALUE_LISTENER_SUCCESS,
+    deckId,
+    deck,
+  }
+}
+
+function settingDeckValueListenerFailure(deckId, error) {
+  return {
+    type: SETTING_DECK_VALUE_LISTENER_FAILURE,
+    deckId,
+    error,
+  }
+}
+
 const initialDeckState = Map({
+  isLoading: true,
+  loadingError: '',
+
   deckId: '',
   name: '',
   cards: Map(),
@@ -35,12 +56,24 @@ const initialDeckState = Map({
 
 function deck(state = initialDeckState, action) {
   switch (action.type) {
-    case UPDATE_DECK_VALUE:
-      return state.merge(action.deck)
     case ADD_DECK_CARD:
       return state.setIn(['cards', action.cardId], true)
     case REMOVE_DECK_CARD:
       return state.deleteIn(['cards', action.cardId])
+    case SETTING_DECK_VALUE_LISTENER:
+      return state
+        .set('isLoading', true)
+        .set('loadingError', '')
+    case SETTING_DECK_VALUE_LISTENER_SUCCESS:
+      // TODO: Can action.deck be null?
+      return state
+        .set('isLoading', false)
+        .set('loadingError', '')
+        .merge(action.deck)
+    case SETTING_DECK_VALUE_LISTENER_FAILURE:
+      return state
+        .set('isLoading', false)
+        .set('loadingError', action.error)
     default:
       return state
   }
@@ -52,11 +85,11 @@ const initialState = Map({
 
 export default function decks(state = initialState, action) {
   switch (action.type) {
-    case UPDATE_DECK_VALUE:
-      const path = ['decks', action.deck.deckId]
-      return state.setIn(path, deck(state.getIn(path), action))
     case ADD_DECK_CARD:
     case REMOVE_DECK_CARD:
+    case SETTING_DECK_VALUE_LISTENER:
+    case SETTING_DECK_VALUE_LISTENER_SUCCESS:
+    case SETTING_DECK_VALUE_LISTENER_FAILURE:
       const path = ['decks', action.deckId]
       return state.setIn(path, deck(state.getIn(path), action))
     default:
