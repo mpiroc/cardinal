081df1e0a805afa97f9e8744702d8a833f1b4371
Matthew Pirocchi <matthew.pirocchi@gmail.com>
2016-12-28 18:48:27 -0500

    Update TODO.

--- app/components/NewCardRTCard/NewCardRTCard.js
+++ app/components/NewCardRTCard/NewCardRTCard.js
@@ -6,7 +6,7 @@ import { NewCardDialogContainer } from 'containers'
 
 export default function NewCardRTCard (props) {
   return (
-    <Card style={{ width: '350px', minHeight: '100px', margin: '0 1.8rem 1.8rem 0', justifyContent: 'center' }}>
+    <Card style={{ width: '50%', minHeight: '100px', margin: '0 1.8rem 1.8rem 0', justifyContent: 'center' }}>
       <CardText style={{display: 'flex', justifyContent: 'center'}}>
         <Button icon='add_circle_outline' label={'Create New Card'} onClick={props.onOpenDialog} />
         <NewCardDialogContainer />
