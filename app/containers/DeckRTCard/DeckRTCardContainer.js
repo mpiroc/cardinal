d34237b3d7c348cca1adfc955cb1c589d91efc6b
Matthew Pirocchi <matthew.pirocchi@gmail.com>
2016-12-29 17:26:20 -0500

    Turn DeckRTCardContainer into thin wrapper.

--- app/containers/DeckRTCard/DeckRTCardContainer.js
+++ app/containers/DeckRTCard/DeckRTCardContainer.js
@@ -1,64 +1,19 @@
 import React, { PropTypes } from 'react'
+import { withRouter } from 'react-router'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { DeckRTCard } from 'components'
 import * as deckActionCreators from 'redux/modules/decks'
 import * as editDeckDialogActionCreators from 'redux/modules/editDeckDialog'
 
-class DeckRTCardContainer extends React.Component {
-  constructor() {
-    super()
-    this.handleViewDeck = this.handleViewDeck.bind(this)
-  }
-  handleViewDeck() {
-    const { deckId } = this.props
-    this.context.router.replace(`deck/${deckId}`)
-  }
-  render () {
-    const {
-      deckId,
-      isDeleting,
-      name,
-      description,
-      onEdit,
-      onDelete,
-    } = this.props
-
-    return (
-      <DeckRTCard
-        deckId={deckId}
-        isDeleting={isDeleting}
-        name={name}
-        description={description}
-        onView={this.handleViewDeck}
-        onEdit={onEdit}
-        onDelete={onDelete}
-      />
-    )
-  }
-}
-
-DeckRTCardContainer.propTypes = {
-  deckId: PropTypes.string.isRequired,
-  uid: PropTypes.string.isRequired,
-  isDeleting: PropTypes.bool.isRequired,
-  name: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  onEdit: PropTypes.func.isRequired,
-  onDelete: PropTypes.func.isRequired,
-}
-
-DeckRTCardContainer.contextTypes = {
-  router: PropTypes.object.isRequired,
-}
-
-function mapStateToProps ({ auth, decks }, ownProps) {
-  const deck = decks.getIn(['decks', ownProps.deckId])
+function mapStateToProps ({ auth, decks }, { deckId, router }) {
+  const deck = decks.getIn(['decks', deckId])
   return {
     uid: auth.get('authedUid'),
     isDeleting: deck.get('isDeleting'),
     name: deck.get('name'),
-    description: deck.get('description')
+    description: deck.get('description'),
+    onView: () => router.replace(`deck/${deckId}`),
   }
 }
 
@@ -85,8 +40,8 @@ function mergeProps (stateProps, dispatchProps, ownProps) {
   }
 }
 
-export default connect(
+export default withRouter(connect(
   mapStateToProps,
   mapDispatchToProps,
   mergeProps,
-)(DeckRTCardContainer)
+)(DeckRTCard))
