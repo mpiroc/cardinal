4f70939801ca956e217d5642e75eb15dfb6374c0
Matthew Pirocchi <matthew.pirocchi@gmail.com>
2017-01-13 18:48:57 -0500

    Fix margins of 'new deck' component.

--- app/components/NewDeckRTCard/NewDeckRTCard.js
+++ app/components/NewDeckRTCard/NewDeckRTCard.js
@@ -6,12 +6,14 @@ import NewDeckDialogContainer from 'containers/NewDeckDialog/NewDeckDialogContai
 
 export default function NewDeckRTCard ({ onOpenDialog }) {
   return (
-    <Card style={{ width: '350px', minHeight: '100px', margin: '0 1.8rem 1.8rem 0', justifyContent: 'center' }}>
-      <CardText style={{display: 'flex', justifyContent: 'center'}}>
-        <Button icon='add_circle_outline' label={'Create New Deck'} onClick={onOpenDialog} />
-        <NewDeckDialogContainer />
-      </CardText>
-    </Card>
+    <div style={{ margin: '1.8rem -0.9rem' }}>
+      <Card style={{ width: '350px', minHeight: '100px', margin: '0 0.9rem', justifyContent: 'center' }}>
+        <CardText style={{display: 'flex', justifyContent: 'center'}}>
+          <Button icon='add_circle_outline' label={'Create New Deck'} onClick={onOpenDialog} />
+          <NewDeckDialogContainer />
+        </CardText>
+      </Card>
+    </div>
   )
 }
 
